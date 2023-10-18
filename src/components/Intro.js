626e51c6d701dc7947ed62b75d6a5a5656679589
ThomasScheepers <thomas@scheepers.com>
2023-10-18 11:49:44 +0200

    Added name

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,6 +7,7 @@ export default function Intro() {
             <div className="taskbar">
                 <p>(o) Intro</p>
             </div>
+            <p>Mijn naam is Thomas Scheepers.</p>
             <p>Ik ben een veelzijdige professional met ruime ICT-ervaring, inclusief netwerken, programmeren, IT-support en computerassemblage. Mijn creatieve vaardigheden en communicatieve bekwaamheid maken me een waardevolle aanwinst in teams en discussies.</p>
             <p className="call-to-action">Communiceer met de <a href="#terminal">terminal</a> om meer te weten!</p>
         </section>
