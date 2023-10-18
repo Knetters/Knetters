29fcb20134b694bd934ac250fb9a2c2a53332641
ThomasScheepers <thomas@scheepers.com>
2023-10-18 17:32:28 +0200

    styling resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,8 +3,27 @@ import React from "react";
 export default function Resume() {
 
     return (
-        <section className="resume-container">
+        <div className="resume-container">
             <h2 className="set-right"><span className="grey">&#60;</span>Resume <span className="grey">/&#62;</span></h2>
-        </section>
+
+            <section className="long-panel intro-panel">
+                <div className="taskbar">
+                    <p>(o) Resume</p>
+                </div>
+                <div className="resume-title">
+                    <h4>Titel</h4>
+                </div>
+                <div className="resume-row">
+                    <div className="resume-item">
+                        <h4>Werk</h4>
+                        <p>Hier komt tekst</p>
+                    </div>
+                    <div className="resume-item">
+                        <h4>Werk</h4>
+                        <p>Hier komt tekst</p>
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
