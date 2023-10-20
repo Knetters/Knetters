0391b0f3b788059e2b7fa108598ca40780288fd2
ThomasScheepers <thomas@scheepers.com>
2023-10-20 14:33:26 +0200

    Spellcheck

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ export default function Banner() {
     return (
         <div>
             <nav>
-                <p><a href="/">Into</a> / <a href="#digital-brain">Digital brain</a> / <a href="#projects">Projects</a> / <a href="#resume">Resume</a></p>
+                <p><a href="/">Intro</a> / <a href="#digital-brain">Digital brain</a> / <a href="#projects">Projects</a> / <a href="#resume">Resume</a></p>
             </nav>
 
             <section className="top-image-container">
