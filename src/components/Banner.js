8be009a18136a460e0dae24e88549b3dd431fb86
ThomasScheepers <thomas@scheepers.com>
2023-10-22 13:45:46 +0200

    Added random images

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Banner() {
 
-    return (
-        <div>
-            <nav>
-                <p><a href="/">Intro</a> / <a href="#digital-brain">Digital brain</a> / <a href="#projects">Projects</a> / <a href="#resume">Resume</a></p>
-            </nav>
+  // List of banner images
+  const bannerImages = ["frog.jpeg", "wizard.jpeg", "dungeon.jpeg"];
 
-            <section className="top-image-container">
+  // State to store the selected image
+  const [selectedImage, setSelectedImage] = useState("");
 
-                {/* Misschien elke keer een random image inladen */}
-                <img className="top-image" src="/img/dungeon.jpeg" alt="" />
-                <div className="gradient-overlay"></div>
-            </section>
-        </div>
-    )
+  // Function to select a random image
+  const getRandomImage = () => {
+    const randomIndex = Math.floor(Math.random() * bannerImages.length);
+    return bannerImages[randomIndex];
+  };
+
+  // Set the initial image when the component mounts
+  useEffect(() => {
+    setSelectedImage(getRandomImage());
+  }, []);
+
+  return (
+    <div>
+      <nav>
+        <p><a href="/">Intro</a> / <a href="#digital-brain">Digital brain</a> / <a href="#projects">Projects</a> / <a href="#resume">Resume</a></p>
+      </nav>
+
+      <section className="top-image-container">
+        {/* Display the selected random image */}
+        <img className="top-image" src={`/img/${selectedImage}`} alt="" />
+        <div className="gradient-overlay"></div>
+      </section>
+    </div>
+  );
 }
