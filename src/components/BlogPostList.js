511204d27625d8ff0664a0e544cf6a49a4e2de8d
Tjips <thomas@scheepers.com>
2023-11-05 22:12:38 +0100

    Added blog list items

--- src/components/BlogPostList.js
+++ src/components/BlogPostList.js
@@ -4,28 +4,58 @@ export default function BlogPostList() {
 
     return (
         <section>    
+        
+            <h3 className="blog-title"><span className="grey">&#60;</span>Posts<span className="grey">/&#62;</span></h3>
+
+            <div className="blog-filter-row">
+                <div className="innder-blog-row">
+                    <ul className="blog-filter-list">
+                        <li>Alles</li>
+                        <li>Webdesign</li>
+                        <li>Ontwikkelingen</li>
+                        <li>Bedrijfsleven</li>
+                    </ul>
+                    <input className="blog-filter-input" type="text" placeholder="Zoeken" />
+                </div>
+            </div>
+
             <ul className="blog-row">
                 <a href="/blog/post/[slug]">
                     <li className="blog-item">
                         <div className="inner-blog-item">
-                            <img src="/img/wizard.jpeg" alt="" />
+                            <img src="/img/bedrijfsleven.png" alt="" />
                             <div className="inner-blog-content">
                                 <div className="blog-title-row">
-                                    <h3><span className="grey">&#60;</span>Titel van een blog <span className="grey">/&#62;</span></h3>
+                                    <h3><span className="grey">&#60;</span>Lange titel van een blog <span className="grey">/&#62;</span></h3>
                                     <p>Gastspreker: <span className="green">Naam gastspreker</span></p>
                                 </div>
+                                <p className="blog-intro-text">Hier komt wat intro tekst van een blog post, geen idee hoe lang dit moet zijn. Misschien maar twee regels?</p>
                             </div>
                         </div>
                     </li>
 
                     <li className="blog-item">
                         <div className="inner-blog-item">
-                            <img src="/img/knight.jpeg" alt="" />
+                            <img src="/img/ontwikkelingen.png" alt="" />
                             <div className="inner-blog-content">
                                 <div className="blog-title-row">
                                     <h3><span className="grey">&#60;</span>Titel van een blog <span className="grey">/&#62;</span></h3>
+                                    <p>Gastspreker: <span className="green">Naam van gastspreker</span></p>
+                                </div>
+                                <p className="blog-intro-text">Hier komt wat intro tekst van een blog post. Misschien maar twee regels?</p>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li className="blog-item">
+                        <div className="inner-blog-item">
+                            <img src="/img/webdesign.png" alt="" />
+                            <div className="inner-blog-content">
+                                <div className="blog-title-row">
+                                    <h3><span className="grey">&#60;</span>Nog langere titel van een blog <span className="grey">/&#62;</span></h3>
                                     <p>Gastspreker: <span className="green">Naam gastspreker</span></p>
                                 </div>
+                                <p className="blog-intro-text">Hier komt wat intro tekst van een blog post, geen idee hoe lang dit moet zijn. Misschien maar twee regels?</p>
                             </div>
                         </div>
                     </li>
