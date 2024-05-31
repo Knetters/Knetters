a70db085b76ef7988dc29cdfbea20c5d2faf283a
Thomas Scheepers <60781257+Knetters@users.noreply.github.com>
2024-05-31 15:40:23 +0200

    Update Terminal.js

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -160,6 +160,7 @@ export default function Terminal() {
                             <p><span class="info-label">Name:</span> <span class="info-description green">Thomas Scheepers</span></p>
                             <p><span class="info-label">Age:</span> <span class="info-description green">24y</span></p>
                             <p><span class="info-label">Gender:</span> <span class="info-description green">Male</span></p>
+                            <p><span class="info-label">Location:</span> <span class="info-description green">Alkmaar, Netherlands</span></p>
                             <p><span class="info-label">Email:</span> <span class="info-description green">thomas@scheepers.com</span></p>
                             <p><span class="info-label">GitHub Name:</span> <span class="info-description yellow">Knetters</span></p>
                         `);
